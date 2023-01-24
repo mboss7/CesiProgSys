7f87a5b012fb2fd0bd6cd1c34549b0a0e1f2a51a
mboss7 <matthias.boss@hotmail.fr>
2023-01-24 13:18:01 +0100

    test CLI

--- AppSauvegarde/AppSauvagardeCLIsrv/ConsoleAppSauvegarde/Program.cs
+++ AppSauvegarde/AppSauvagardeCLIsrv/ConsoleAppSauvegarde/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+namespace HelloWorld;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
