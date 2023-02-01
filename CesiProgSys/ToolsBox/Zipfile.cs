6926e21ec98e4ad2e68ea9c83c90413f56117e3d
Louis <dionisagrokid@hotmail.com>
2023-02-01 09:58:24 +0100

    Zipfile create manque les paramètres

---
+++ CesiProgSys/ToolsBox/Zipfile.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CesiProgSys.ToolsBox
+{
+    public class Zipfile
+    {
+        public static void Main()
+        {
+            string outputFile = "";
+            string fileToZip = "";
+            using (var archive = ZipFile.Open(outputFile, ZipArchiveMode.Create)) 
+            {
+                archive.CreateEntryFromFile(fileToZip, Path.GetFileName(fileToZip));
+            }
+
+        }
+    }
+}
