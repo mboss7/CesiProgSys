c037d038fc6addc2d0fb02e9ec3662263600ef8f
Louis <dionisagrokid@hotmail.com>
2023-02-02 09:14:13 +0100

    json avec paramètre à modifier

---
+++ CesiProgSys/ToolsBox/Jsoncreator2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+
+public class BackupInfo
+{
+    public DateTime Timestamp { get; set; }
+    public string BackupName { get; set; }
+    public string SourceFileAddress { get; set; }
+    public string DestinationFileAddress { get; set; }
+    public long FileSize { get; set; }
+    public long TransferTime { get; set; }
+
+    public BackupInfo(DateTime timestamp, string backupName, string sourceFileAddress, string destinationFileAddress, long fileSize, long transferTime)
+    {
+        Timestamp = timestamp;
+        BackupName = backupName;
+        SourceFileAddress = sourceFileAddress;
+        DestinationFileAddress = destinationFileAddress;
+        FileSize = fileSize;
+        TransferTime = transferTime;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BackupInfo backup = new BackupInfo(DateTime.Now, "Sample_log.pdf [pdf]", @"\\server\source\Sample_log.pdf", @"\\server\destination\Sample_log.pdf", 10000, 500);
+        string json = JsonConvert.SerializeObject(backup);
+        Console.WriteLine(json);
+    }
+}
