57db03fdfbaad052677b7ba5219c942e68b9d21c
mboss7 <matthias.boss@hotmail.fr>
2023-02-03 09:38:17 +0100

    Modify Program.cs

--- CesiProgSys/Program.cs
+++ CesiProgSys/Program.cs
@@ -1,11 +1,28 @@
 
 using CesiProgSys.ToolsBox;
+using CesiProgSys.LOG;
 
 namespace CesiProgSys
 {
     class Program
     {
-        static void Main(string[] args)
+        
+        
+        public class LOGS
+        {
+            public static void Main()
+            {
+                // run new LogsManager Thread
+                LogsManager l = new LogsManager();
+                l.instantiate();
+            
+                // sleep 10 sec et finish le Thread LogsManager
+                Thread.Sleep(10000);
+                l.finish();
+           
+            }
+        }
+        static void MainC(string[] args)
         {
             Zip z = new Zip();
             z.compressed(args[0], args[1]);
@@ -15,3 +32,7 @@ namespace CesiProgSys
         
     }
 }
+
+
+
+  
