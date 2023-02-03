94bbfb53a4e248fe9019372cfebe7cf6266fdbdd
Louis <dionisagrokid@hotmail.com>
2023-02-03 22:54:12 +0100

    hash en sha256

--- CesiProgSys/ToolsBox/Hash.cs
+++ CesiProgSys/ToolsBox/Hash.cs
@@ -1,16 +1,73 @@
-﻿// using System.Security.Cryptography;
-//
-// namespace CesiProgSys.ToolsBox;
-//
-// // Method to get file name and hash combined of a file
-// public string GetFileNameAndMD5Hash(FileInfo file)
-// {
-//     using (var md5 = MD5.Create())
-//     {
-//         using (var stream = File.OpenRead(file.FullName))
-//         {
-//             var hash = md5.ComputeHash(stream);
-//             return (file.Name+BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant());
-//         }
-//     }
-// }
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace FileIntegrityChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The file path to be checked
+            string filePath = @"C:\path\to\file.txt";
+
+            // Calculate the hash of the file before saving
+            byte[] originalHash = CalculateHash(filePath);
+
+            // Save the file
+            // ...
+
+            // Calculate the hash of the file after saving
+            byte[] savedHash = CalculateHash(filePath);
+
+            // Compare the two hashes
+            if (CompareHashes(originalHash, savedHash))
+            {
+                Console.WriteLine("The files are identical.");
+            }
+            else
+            {
+                Console.WriteLine("The files are different.");
+            }
+
+            Console.ReadKey();
+        }
+
+        // Calculates the hash of a file using SHA256
+        static byte[] CalculateHash(string filePath)
+        {
+            // Create an instance of SHA256
+            using (var sha256 = SHA256.Create())
+            {
+                // Open the file
+                using (var stream = File.OpenRead(filePath))
+                {
+                    // Compute the hash of the file contents
+                    return sha256.ComputeHash(stream);
+                }
+            }
+        }
+
+        // Compares two hashes
+        static bool CompareHashes(byte[] first, byte[] second)
+        {
+            // If the lengths of the two arrays are different, the hashes are different
+            if (first.Length != second.Length)
+            {
+                return false;
+            }
+
+            // Compare the contents of the two arrays
+            for (int i = 0; i < first.Length; i++)
+            {
+                if (first[i] != second[i])
+                {
+                    return false;
+                }
+            }
+
+            // If all elements are equal, the hashes are equal
+            return true;
+        }
+    }
+}
