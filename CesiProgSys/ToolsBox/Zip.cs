6d032c66c18803d44c5d2f173594ed8730113cac
Louis <dionisagrokid@hotmail.com>
2023-02-06 14:03:48 +0100

    commentaire en english

--- CesiProgSys/ToolsBox/Zip.cs
+++ CesiProgSys/ToolsBox/Zip.cs
@@ -1,4 +1,4 @@
-﻿
+﻿// Importing necessary libraries
 using System;
 using System.Collections.Generic;
 using System.IO.Compression;
@@ -8,21 +8,29 @@ using System.Threading.Tasks;
 
 namespace CesiProgSys.ToolsBox
 {
+    // Class for Zip compression
     public class Zip
     {
+        // Default constructor
         public Zip(){}
+
+        // Method to compress a directory
         public void compressed(string location, string file)
         {
-            
+            // Compress the directory and save the compressed file
             ZipFile.CreateFromDirectory(location, file);
+            
+            // Check if the file exists
             string path = "C:\\Files\\file.txt";
             bool result = File.Exists(path);
             if (result == true)
             {
+                // If file found, print message
                 Console.WriteLine("File Found");
             }
             else
             {
+                // If file not found, print message
                 Console.WriteLine("File Not Found");
             }
         }
