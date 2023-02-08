d79d1ffc44b83847376e21b24f92867263ee4a6e
TanguyGackel <tanguy.gackel@gmail.com>
2023-02-08 12:15:38 +0100

    finit de refactor logsmanager

--- CesiProgSys/LOG/LogsManager.cs
+++ CesiProgSys/LOG/LogsManager.cs
@@ -5,7 +5,6 @@ namespace CesiProgSys.LOG
 	//mother class for Logs 
 	public class LogsManager //: IManager
 	{
-
 		public void startLogManager()
 		{
 			// run new LogsManager Thread
@@ -14,12 +13,6 @@ namespace CesiProgSys.LOG
 			// l.finish();
 		}
 		
-		// thread list, for managing Thread
-		public List<Thread> threadList;
-		// constructor : for thread list
-		public LogsManager() {
-			threadList = new List<Thread>();
-		}
 		// methode for create new thread 
 		public void instantiate()
 		{
@@ -27,15 +20,11 @@ namespace CesiProgSys.LOG
             Thread instanceRtl = new Thread(RealTimeLogs.startThread);
             //  Console.Write("{0}\n", Thread.CurrentThread.ManagedThreadId);
             instanceRtl.Start();
-            threadList.Add(instanceRtl);
             
             // create thread for DailyLogs Object
             Thread instanceDl = new Thread(DailyLogs.startThread);
             //  Console.Write("{0}\n", Thread.CurrentThread.ManagedThreadId);
             instanceDl.Start();
-            threadList.Add(instanceDl);
-            
-            Thread.Sleep(10);
 		}
 
 		// methode for end current thread
