5665dd73b8d6f78779fbc5dac43be13a732857cc
mboss7 <matthias.boss@hotmail.fr>
2023-02-08 09:43:51 +0100

    Add Avalonia View init

---
+++ CesiProgSys/View_Avalonia/AvaloniaIHM_View/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaIHM_View
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
