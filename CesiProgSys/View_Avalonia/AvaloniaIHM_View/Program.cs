0ca5ba7009b4433111be8219176398d1f2069c0a
mboss7 <matthias.boss@hotmail.fr>
2023-02-09 10:45:34 +0100

    test debug client tcp

--- CesiProgSys/View_Avalonia/AvaloniaIHM_View/Program.cs
+++ CesiProgSys/View_Avalonia/AvaloniaIHM_View/Program.cs
@@ -4,23 +4,36 @@ using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace AvaloniaIHM_View
 {
 
     public class Program
     {
+        public static extern int SetStdHandle(int device, IntPtr handle); 
 
         static void Main()
         {
             Console.WriteLine("It's Works ? ");
-          
+           // Console.Out.Flush(); 
             // run TCP CLIENT : 
-            TcpLinkClient tcpLinstenok = new TcpLinkClient();
+            //TcpLinkClient tcpLinstenok = new TcpLinkClient();
 
-            tcpLinstenok.tcpClient();
+           // tcpLinstenok.tcpClient();
 
-            Console.WriteLine("It's Works ! ");
+          // FileStream fileStream = new FileStream("logfile.txt", FileMode.Create);
+          // StreamWriter streamwriter = new StreamWriter(fileStream);
+           
+          // Console.SetOut(streamwriter);
+
+          // IntPtr handle;
+          // handle = fileStream.Handle;
+
+          // int status = SetStdHandle(-11, handle);
+           
+
+           Console.WriteLine("It's Works ! ");
             Console.ReadLine();
 
 
