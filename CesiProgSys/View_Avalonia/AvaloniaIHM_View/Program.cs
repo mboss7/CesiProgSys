629b2a3e63725dfade9ac6bb38b85d9e1ddaea6b
mboss7 <matthias.boss@hotmail.fr>
2023-02-09 11:03:03 +0100

    client tcp prog

--- CesiProgSys/View_Avalonia/AvaloniaIHM_View/Program.cs
+++ CesiProgSys/View_Avalonia/AvaloniaIHM_View/Program.cs
@@ -11,42 +11,19 @@ namespace AvaloniaIHM_View
 
     public class Program
     {
-       // public static extern int SetStdHandle(int device, IntPtr handle); 
-
+      
         static void Main()
         {
             Console.WriteLine("It's Works ? ");
-           // Console.Out.Flush(); 
-            // run TCP CLIENT : 
-            //TcpLinkClient tcpLinstenok = new TcpLinkClient();
+           // run TCP CLIENT : 
+            TcpLinkClient tcpLinstenok = new TcpLinkClient();
+            tcpLinstenok.tcpClient();
 
-           // tcpLinstenok.tcpClient();
-
-           
-           
-           // test sortie standard
-          // FileStream fileStream = new FileStream("logfile.txt", FileMode.Create);
-          // StreamWriter streamwriter = new StreamWriter(fileStream);
-           
-          // Console.SetOut(streamwriter);
-
-          // IntPtr handle;
-          // handle = fileStream.Handle;
-
-          // int status = SetStdHandle(-11, handle);
-           
-
-           Console.WriteLine("It's Works ! ");
+            Console.WriteLine("It's Works ! ");
             Console.ReadLine();
 
 
             // Run Avalonia 
-
-
-
-
-
-
         }
     }
 }
