aa0c9b1f20932651557e6633d56d0214c3b54bae
mboss7 <matthias.boss@hotmail.fr>
2023-02-13 22:28:19 +0100

    First commit test ssl need to be debug

---
+++ TCP_SSL/TCP_SSL_EXE/TcpClient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+
+namespace TCP_SSL_EXE
+{
+    public class TcpClient
+    {
+        public void ClientRun()
+        {
+            // Charger le certificat SSL/TLS
+            X509Certificate2 certificate = new X509Certificate2("cert.pfx", "password");
+
+            // Créer une socket TCP et la configurer pour se connecter au serveur
+            TcpClient client = new TcpClient("localhost", 12345);
+
+            // Créer un stream SSL/TLS sécurisé
+            SslStream sslStream = new SslStream(client.GetStream(), false);
+            sslStream.AuthenticateAsClient("localhost", new X509Certificate2[] { certificate }, SslProtocols.Tls, true);
+
+            // Envoyer le nom de fichier au serveur
+            string filename = "example.txt";
+            byte[] filenameBuffer = Encoding.ASCII.GetBytes(filename);
+            sslStream.Write(filenameBuffer, 0, filenameBuffer.Length);
+
+            // Recevoir les données du fichier à partir du serveur
+            byte[] fileData = new byte[1024];
+            MemoryStream memoryStream = new MemoryStream();
+            int bytesRead;
+            while ((bytesRead = sslStream.Read(fileData, 0, fileData.Length)) > 0)
+            {
+                memoryStream.Write(fileData, 0, bytesRead);
+            }
+
+            // Écrire les données du fichier dans un fichier local
+            File.WriteAllBytes(filename, memoryStream.ToArray());
+
+            // Fermer la connexion sécurisée
+            sslStream.Close();
+            client.Close();
+        }
+    }
+    }
+}
+
---
+++ TCP_SSL/TCP_SSL_EXE/TcpServer.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace TCP_SSL_EXE
+{
+    public class TcpServer
+    
+    {
+
+        public void ServerRun()
+        {
+            // Charger le certificat SSL/TLS
+            X509Certificate2 certificate = new X509Certificate2("cert.pfx", "password");
+
+            // Créer une socket TCP et la configurer pour écouter les connexions entrantes
+            TcpListener listener = new TcpListener(IPAddress.Any, 12345);
+            listener.Start();
+
+            // Boucle infinie pour accepter les connexions entrantes
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+
+                // Créer un stream SSL/TLS sécurisé
+                SslStream sslStream = new SslStream(client.GetStream(), false);
+                sslStream.AuthenticateAsServer(certificate, false, SslProtocols.Tls, true);
+
+                // Recevoir le nom de fichier à partir du client
+                byte[] filenameBuffer = new byte[1024];
+                int filenameLength = sslStream.Read(filenameBuffer, 0, filenameBuffer.Length);
+                string filename = Encoding.ASCII.GetString(filenameBuffer, 0, filenameLength);
+
+                // Ouvrir le fichier et le lire
+                byte[] fileData = File.ReadAllBytes(filename);
+
+                // Envoyer les données du fichier au client
+                sslStream.Write(fileData, 0, fileData.Length);
+
+                // Fermer la connexion sécurisée
+                sslStream.Close();
+                client.Close();
+            }
+        }
+    }
+}
+
+}
+
+
