1e096baad69b5e86ad73dbaeeced36c5f7a42d4b
mboss7 <matthias.boss@hotmail.fr>
2023-02-13 12:43:12 +0100

    all

---
+++ CRYPTOSOFT_EXE/CryptosoftEXE/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace XOREncryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ViewCryptosoft vue = new ViewCryptosoft();
+            
+            vue.ViewLogiciel();
+        }
+    }
+}
