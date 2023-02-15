fa821c362a5c99fdc1802a80e20976abc38e4aed
mboss7 <matthias.boss@hotmail.fr>
2023-02-15 09:52:06 +0100

    maj git test init

---
+++ TCP_SSL/Client _Tcp_SSL/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Sockets;
+
+
+// certif chiffrement  asymétrique en aes : https://learn.microsoft.com/en-us/dotnet/api/System.Security.Cryptography.X509Certificates.X509Certificate2?view=net-7.0
+//
+
+namespace Tcp_Ssl
+{
+    public class program
+    {
+        static void Main(string[] args)
+
+        {
+            
+            TcpClientSsl tcpClient = new TcpClientSsl();
+
+
+            Console.WriteLine("Start \n");
+           
+            tcpClient.TcpClientRun();
+
+        }
+    }
+}
---
+++ TCP_SSL/Client _Tcp_SSL/TcpClientSsl.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Tcp_Ssl
+{
+    public class TcpClientSsl
+    {
+        public void TcpClientRun()
+        {
+            try
+            {
+                // Établir une connexion sur le Port 9999 
+                IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ip = host.AddressList[0];
+                IPEndPoint endPoint = new IPEndPoint(ip, 9999);
+
+                // Créer une socket TCP/IP 
+                Socket client = new Socket(ip.AddressFamily, SocketType.Stream,
+                    ProtocolType.Tcp);
+
+                try
+                {
+                    // Connect Socket 
+                    client.Connect(endPoint);
+
+                    // le message à envoyer au serveur
+                    byte[] msg = Encoding.ASCII.GetBytes("Hello<EOF>");
+                    int byteSent = client.Send(msg);
+
+                    // Data buffer 
+                    byte[] messageReceived = new byte[1024];
+
+                    // Recevoir le message 
+                    int byteRecv = client.Receive(messageReceived);
+                    Console.WriteLine("Message du serveur -> {0}",
+                        Encoding.ASCII.GetString(messageReceived, 0, byteRecv));
+
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+                }
+                catch (SocketException e1)
+                {
+                    Console.WriteLine("SocketException : {0}", e1.ToString());
+                }
+                catch (ArgumentNullException e2)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", e2.ToString());
+                }
+                catch (Exception e3)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e3.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
