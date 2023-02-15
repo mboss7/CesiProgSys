d5147e9629a9716ffc6ab8c54b0926d1a82da7b0
Louis <dionisagrokid@hotmail.com>
2023-02-15 10:40:30 +0100

    Ajoutez des fichiers projet.

---
+++ AvaloniaApplication2/MainWindow.axaml.cs
@@ -0,0 +1,16 @@
+using Avalonia.Controls;
+
+namespace AvaloniaApplication2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void OnClickCommand()
+        {
+            // do something
+        }
+    }
+}
