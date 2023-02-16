f7bcd08ed831ed9469b1723bee2fde7a6da039f3
mboss7 <matthias.boss@hotmail.fr>
2023-02-16 17:55:20 +0100

    modify working ssl connection

--- TCP_SSL/Client _Tcp_SSL/TcpClientSsl.cs
+++ TCP_SSL/Client _Tcp_SSL/TcpClientSsl.cs
@@ -3,11 +3,19 @@ using System.Net.Sockets;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 
+namespace Tcp_Ssl
+{
+    public class TcpClientSsl
+    {
+        public void SslTcpClientConnection()
+        {
+            X509Certificate2Collection clientCertificates = new X509Certificate2Collection();
+            clientCertificates.Add(new X509Certificate2(@"cert.pfx", "P@ssw0rd"));
+            TcpClient client = new TcpClient("localhost", 1234);
+            SslStream sslStream = new SslStream(client.GetStream(), false, (sender, certificate, chain, errors) => true);
+            sslStream.AuthenticateAsClient("adresse IP ou nom de domaine du serveur", clientCertificates, SslProtocols.Tls, true);
+        }
+    }
+}
 
-
-X509Certificate2Collection clientCertificates = new X509Certificate2Collection();
-clientCertificates.Add(new X509Certificate2("chemin/vers/le/certificat.pfx", "mot de passe"));
-TcpClient client = new TcpClient("adresse IP ou nom de domaine du serveur", 1234);
-SslStream sslStream = new SslStream(client.GetStream(), false, (sender, certificate, chain, errors) => true);
-sslStream.AuthenticateAsClient("adresse IP ou nom de domaine du serveur", clientCertificates, SslProtocols.Tls, true);
 
--- TCP_SSL/TCP_SSL_EXE/TcpServer.cs
+++ TCP_SSL/TCP_SSL_EXE/TcpServer.cs
@@ -4,9 +4,21 @@ using System.Net;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 
-X509Certificate serverCertificate = new X509Certificate(@"cert.pfx", "P@ssw0rd");
-TcpListener listener = new TcpListener(IPAddress.Any, 1234);
-listener.Start();
-TcpClient client = listener.AcceptTcpClient();
-SslStream sslStream = new SslStream(client.GetStream(), false);
-sslStream.AuthenticateAsServer(serverCertificate, false, SslProtocols.Tls, true);
+namespace Tcp_Ssl
+{
+    public class TcpServer
+    {
+        public void SslTcpServerConnection()
+        {
+        
+            X509Certificate serverCertificate = new X509Certificate(@"cert.pfx", "P@ssw0rd");
+            TcpListener listener = new TcpListener(IPAddress.Any, 1234);
+            listener.Start();
+            TcpClient client = listener.AcceptTcpClient();
+            SslStream sslStream = new SslStream(client.GetStream(), false);
+            sslStream.AuthenticateAsServer(serverCertificate, false, SslProtocols.Tls, true);
+        }
+    }
+ 
+}
+
