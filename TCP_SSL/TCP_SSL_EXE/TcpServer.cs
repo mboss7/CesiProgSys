5ebe206233aef326b7b1ce12dae612a28a5ed20a
mboss7 <matthias.boss@hotmail.fr>
2023-02-16 17:15:57 +0100

    srv works

--- TCP_SSL/TCP_SSL_EXE/TcpServer.cs
+++ TCP_SSL/TCP_SSL_EXE/TcpServer.cs
@@ -4,7 +4,7 @@ using System.Net;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 
-X509Certificate serverCertificate = new X509Certificate("chemin/vers/le/certificat.pfx", "mot de passe");
+X509Certificate serverCertificate = new X509Certificate(@"cert.pfx", "P@ssw0rd");
 TcpListener listener = new TcpListener(IPAddress.Any, 1234);
 listener.Start();
 TcpClient client = listener.AcceptTcpClient();
