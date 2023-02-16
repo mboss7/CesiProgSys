4dd0846d10c56e5876764af1a49c49eb2920aa1d
mboss7 <matthias.boss@hotmail.fr>
2023-02-16 22:06:03 +0100

    i init commit

---
+++ TCP_SSL/TCP_SSL_EXE/ConditionIHM.cs
@@ -0,0 +1,19 @@
+﻿namespace Tcp_Ssl;
+
+public class ConditionIHM
+{
+    public void IHMServer(string EntreeUtilisateurClient)
+    {
+        switch (EntreeUtilisateurClient)
+        {
+            case "1":
+                break;
+            case "2":
+                break;
+            default:
+                Console.WriteLine("Command unknow !");
+                break;
+        }
+        
+    }
+}
