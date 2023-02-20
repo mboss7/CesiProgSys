2b06e7682d9811d1dc64e97d919d223e3a766717
Louis <dionisagrokid@hotmail.com>
2023-02-20 21:50:43 +0100

    open folder file

--- AvaloniaApplication2/MainWindow.axaml.cs
+++ AvaloniaApplication2/MainWindow.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System;
 
 namespace AvaloniaApplication2
 {
@@ -25,6 +26,35 @@ namespace AvaloniaApplication2
             dialog.ShowDialog(this);
         }
 
+        private async void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.AllowMultiple = false;
+            dialog.Directory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            var result = await dialog.ShowAsync(this);
+
+            if (result != null && result.Length == 1)
+            {
+                string filePath = result[0];
+                // Do something with the file path...
+            }
+        }
+
+        private async void OpenFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFolderDialog();
+            dialog.Directory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            var result = await dialog.ShowAsync(this);
+
+            if (result != null)
+            {
+                string folderPath = result;
+                // Do something with the folder path...
+            }
+
+        }
 
     }
    
