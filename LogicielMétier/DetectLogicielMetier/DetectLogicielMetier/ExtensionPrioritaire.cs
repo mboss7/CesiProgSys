744f6a1173dad6f519cd04e3d5ae90fd5f0b1c1e
mboss7 <matthias.boss@hotmail.fr>
2023-02-21 10:44:13 +0100

    avec methode séparées

--- LogicielMétier/DetectLogicielMetier/DetectLogicielMetier/ExtensionPrioritaire.cs
+++ LogicielMétier/DetectLogicielMetier/DetectLogicielMetier/ExtensionPrioritaire.cs
@@ -7,36 +7,52 @@ namespace DetectLogicielMetier
     public class ExtensionPrioritaire
     {
     // list tuple  qui contient  string / liste file info 
-    
-    public void ListeMaker() {
+    private List<string> ListeAvantTri;
 
-            // Créer une liste 
-            List<string> ListeAvantTri = new List<string> ();
-            List<string> fichiersPrioritaires = new List<string> ();
+    private List<string> fichiersPrioritaires;
 
-            // Ajouter des éléments à la liste 
-            ListeAvantTri.Add("Java.exe");
-            ListeAvantTri.Add("Python.txt");
-            ListeAvantTri.Add("C#.lol");
-            ListeAvantTri.Add("PHP.xd");
-            ListeAvantTri.Add("C++.exe");
-            ListeAvantTri.Add("SQL.txt");
+    public void ListeMaker()
+    {
 
-            Console.WriteLine("\nParcourir la liste avec la boucle for-each. \n");
-            foreach(string item in ListeAvantTri) {
-                if (item.Contains(".txt"))
-                {
+        // Créer une liste 
+        List<string> ListeAvantTri = new List<string>();
+        List<string> fichiersPrioritaires = new List<string>();
+
+        // Ajouter des éléments à la liste 
+        ListeAvantTri.Add("Java.exe");
+        ListeAvantTri.Add("Python.txt");
+        ListeAvantTri.Add("C#.lol");
+        ListeAvantTri.Add("PHP.xd");
+        ListeAvantTri.Add("C++.exe");
+        ListeAvantTri.Add("SQL.txt");
+
+    }
+
+    public void ListeTri()
+            {
+                Console.WriteLine("\nParcourir la liste avec la boucle for-each. \n");
+                foreach(string item in ListeAvantTri) {
+                    if (item.Contains(".txt"))
+                    {
                     
-                    fichiersPrioritaires.Add(item);
+                        fichiersPrioritaires.Add(item);
                     
+                    }
                 }
             }
+
+    public void ListReader()
+    {
+           
             
-            Console.WriteLine("\nParcourir la liste element prioritaires avec la boucle for-each:\n ");
-            foreach(string item in fichiersPrioritaires) {
-               Console.WriteLine(item);
-            }
+        Console.WriteLine("\nParcourir la liste element prioritaires avec la boucle for-each:\n ");
+        foreach(string item in fichiersPrioritaires) {
+            Console.WriteLine(item);
+        }
+    }
+
+         
     }
     }
-}
+
 
