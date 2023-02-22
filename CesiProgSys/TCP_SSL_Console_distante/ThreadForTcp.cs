3cc5645318bf189c8969bc39918179ecaf978015
mboss7 <matthias.boss@hotmail.fr>
2023-02-22 10:49:36 +0100

    correct thread tcp

--- CesiProgSys/TCP_SSL_Console_distante/ThreadForTcp.cs
+++ CesiProgSys/TCP_SSL_Console_distante/ThreadForTcp.cs
@@ -2,17 +2,19 @@ namespace Tcp_Ssl
 {
     public class ThreadForTcp
     {
-        /// <summary>
-        /// Run two thread, one client and one server for the model or the view depend exeptation 
-        /// </summary>
-        public void ThreadForTcpSTART()
+      /// <summary>
+      /// Run two thread, one client and one server for the model or the view depend exeptation 
+      /// </summary>
+      /// <param name="IpPort : string[] ip and port"></param>
+        public void ThreadForTcpSTART(string[] IpPort)
         {
             //tableau de string Ip et Port 
-            string[] IpPort = { "localhost", "1234" };
+            //string[] IpPort = { "localhost", "1234" };  // it was for test 
+            int port = int.Parse(IpPort[1]);
 
             // run thread for run server
             Thread tcpServerModel = new Thread(TcpServer.RunSrv);
-            tcpServerModel.Start(1234);
+            tcpServerModel.Start(port);
             // run thread for run client
             Thread tcpClientModel = new Thread(TcpClientSsl.RunClient);
             tcpClientModel.Start(IpPort);
