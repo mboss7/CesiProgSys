1646a84ee484e5bd9574f1ec6fd2921fcc101a51
TanguyGackel <tanguy.gackel@gmail.com>
2023-02-22 09:10:34 +0100

    commit

--- CesiProgSys/Program.cs
+++ CesiProgSys/Program.cs
@@ -1,58 +1,50 @@
-using Avalonia;
 using CesiProgSys.Backups;
 using CesiProgSys.LOG;
 using CesiProgSys.ToolsBox;
 using CesiProgSys.ViewCli;
-using CesiProgSys.ViewAvalonia;
 
 namespace CesiProgSys
 {
 
     public class Program
     {
-        [STAThread]
         static void Main(string[] args)
         {
             bool result;
             new Mutex(true, "ID", out result);
             if (!result) return;
 
-            BackupManager.Instance();
-            LogsManager l = LogsManager.Instance();
-            l.instantiate();
+            // BackupManager.Instance();
+            // LogsManager l = LogsManager.Instance();
+            // l.instantiate();
 
-            Config c = Config.Instance();
-            c.setConfig();
-            c.checkTimeRecentSave();
+            // Config c = Config.Instance();
+            // c.setConfig();
+            // c.checkTimeRecentSave();
 
-            if(args.Any())
-                if (args[0] == "cli")
-                {
-                    Console.WriteLine("\n -----Welcome to the application !----- \n");
-
-                    Console.ReadKey();
-
-                    if (c.language == Language.English)
-                    {
-                        ViewCli.ViewCli objEn = new ViewCliEn();
-                        objEn.menu();
-                    }
-                    else if (c.language == Language.French)
-                    {
-                        ViewCli.ViewCli objFr = new ViewCliFr();
-                        objFr.menu();
-                    }
-                    else
-                    {
-                        ViewCli.ViewCli objEn = new ViewCliEn();
-                        objEn.menu();
-                    }
-                }
-            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+            // if(args.Any())
+            //     if (args[0] == "cli")
+            //     {
+            //         Console.WriteLine("\n -----Welcome to the application !----- \n");
+            //
+            //         Console.ReadKey();
+            //
+            //         if (c.language == Language.English)
+            //         {
+            //             ViewCli.ViewCli objEn = new ViewCliEn();
+            //             objEn.menu();
+            //         }
+            //         else if (c.language == Language.French)
+            //         {
+            //             ViewCli.ViewCli objFr = new ViewCliFr();
+            //             objFr.menu();
+            //         }
+            //         else
+            //         {
+            //             ViewCli.ViewCli objEn = new ViewCliEn();
+            //             objEn.menu();
+                    // }
+                // }
         }
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-                .LogToTrace();
     }
 }
---
+++ ViewAvalonia/Program.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia;
+using ViewAvalonia;
+
+namespace ViewAvalonia;
+
+public class Program
+{
+    [STAThread]
+    static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace();
+}
